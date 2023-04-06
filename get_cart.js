fa8a744b81f6fa8ab3d56d124390255352644471
jpackard58 <118028479+jpackard58@users.noreply.github.com>
2023-04-06 02:49:53 -0400

    Update get_cart.js

--- get_cart.js
+++ get_cart.js
@@ -1,15 +1,19 @@
 function checkout(){
     const cartDiv = document.getElementById("cart");
     const itemContainers = cartDiv.querySelectorAll(".item-container");
+
     active_cart = [];
     itemContainers.forEach(itemContainer => {
         let item = [];
+        let price = itemContainer.querySelector('h2').innerText;
         const itemId = itemContainer.id;
         item.push(itemId);
+        item.push(price);
         const quantitySpan = document.getElementById(itemId[0]+'q');
         const quantity = parseInt(quantitySpan.innerHTML);
         item.push(quantity);
         active_cart.push(item);
     });
+    
     console.log(active_cart);
 };
