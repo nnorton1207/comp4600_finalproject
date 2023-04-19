f38c80f76a8009d12969b4a25bf91c89fe2ecd53
jpackard58 <118028479+jpackard58@users.noreply.github.com>
2023-04-19 01:44:25 -0400

    Update get_cart.js

--- get_cart.js
+++ get_cart.js
@@ -100,8 +100,8 @@ function checkout_form(chkcart) {
     event.preventDefault(); // Prevent the form from submitting normally
     
     AWS.config.update({
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        accessKeyId: env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: env.AWS_SECRET_ACCESS_KEY,
         region: 'us-east-1'
     });    
     // Create a new DynamoDB instance
